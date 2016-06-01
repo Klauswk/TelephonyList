c3d9a63681e9abdb5ed0d4a1f73f22644eab6a1e
Klaus Wiltgen Klein <klauswk1@hotmail.com>
2016-05-31 22:01:03 -0300

    Code refactored

---
+++ public/contatosData.js
@@ -0,0 +1,3 @@
+[{"nome": "Bruno", "telefone": "9999-2222", "data": "new Date()", "operadora":{ "operadora": "Oi", "codigo": 14, "categoria": "Celular"}},
+    {"nome": "Sandra", "telefone": "9999-3333", "data": "new Date()", "operadora": {"operadora": "Vivo", "codigo": 15, "categoria": "Celular"}},
+    {"nome": "Mariana", "telefone": "9999-9999", "data": "new Date()", "operadora": {"operadora": "Tim", "codigo": 41, "categoria": "Celular"}}]
---
+++ public/operadorasData.js
@@ -0,0 +1,3 @@
+[{"operadora": "Tim", "id": "01", "categoria": "Local"},
+{"operadora": "Claro", "id": "02", "categoria": "Local"},
+{"operadora": "Oi", "id": "03", "categoria": "Fixo"}]
